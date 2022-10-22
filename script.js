d92433bca4226e8c29570a2bff99ba9b97a65342
Endzelisp <endzelisp@gmail.com>
2022-10-21 22:28:53 -0400

    Create js const to capture form elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict"
+
+const signUpForm = document.querySelector('.main-container__form > form');
+const passwordEl = signUpForm.querySelector('input[name="password"]');
+const passwordCheckEl = signUpForm.querySelector('input[name="password-check"]');
+const submitBtnEl = signUpForm.querySelector('button[type="submit"]');
