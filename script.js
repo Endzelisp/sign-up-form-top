e3d3fe37566c67dc40d5158d66c23c40135c5588
Endzelisp <endzelisp@gmail.com>
2022-10-22 07:39:08 -0400

    Add password verification function

--- script.js
+++ script.js
@@ -3,4 +3,17 @@
 const signUpForm = document.querySelector('.main-container__form > form');
 const passwordEl = signUpForm.querySelector('input[name="password"]');
 const passwordCheckEl = signUpForm.querySelector('input[name="password-check"]');
+const verificationMessage = signUpForm.querySelector('input[name="password-check"] + span');
 const submitBtnEl = signUpForm.querySelector('button[type="submit"]');
+
+function passwordCheck () {
+  return passwordEl.value === passwordCheckEl.value;
+};
+
+passwordCheckEl.addEventListener('input', () => {
+  if (passwordCheck()) {
+    verificationMessage.textContent = `Password match`;
+  } else {
+    verificationMessage.textContent = `Password didn't match`;
+  }
+});
